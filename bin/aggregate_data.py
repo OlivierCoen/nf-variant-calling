794984ea57fa07f574640fbef0dbe95a98df03ff
Olivier Coen <coen.olivier@gmail.com>
2026-04-25 07:53:21 +0200

    simplify aggregation

--- bin/aggregate_data.py
+++ bin/aggregate_data.py
@@ -7,7 +7,9 @@ from pathlib import Path
 import polars as pl
 from tqdm import tqdm
 
-pl.Config.set_streaming_chunk_size(1e6)
+from common import parse_vcf_data, get_position_in_format
+
+pl.Config.set_streaming_chunk_size(int(1e6))
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,6 +18,8 @@ GROUPED_VARIANTS_OUTFILE_SUFFIX = "grouped_variants.parquet"
 VARIANTS_OUTFILE_SUFFIX = "formated_variants.parquet"
 
 QUANTILE = 0.05
+
+logger.info(f"Polars pool size: {pl.thread_pool_size()}")
 
 
 #####################################################
@@ -28,11 +32,11 @@ QUANTILE = 0.05
 def parse_args():
     parser = argparse.ArgumentParser(description="Aggregate and filter VCF files")
     parser.add_argument(
-        "--variants",
+        "--vcf",
         type=Path,
-        dest="variant_file",
+        dest="vcf_file",
         required=True,
-        help="Path to variant file",
+        help="Path to VCF file",
     )
     parser.add_argument(
         "--pvalues",
@@ -40,27 +44,6 @@ def parse_args():
         dest="pvalue_file",
         required=True,
         help="Path to file containing p-values",
-    )
-    parser.add_argument(
-        "--RO",
-        type=Path,
-        dest="RO_file",
-        required=True,
-        help="Path to file containing reference counts",
-    )
-    parser.add_argument(
-        "--AO",
-        type=Path,
-        dest="AO_file",
-        required=True,
-        help="Path to file containing reference counts",
-    )
-    parser.add_argument(
-        "--design",
-        type=Path,
-        dest="design_file",
-        required=True,
-        help="Path to design file",
     )
     parser.add_argument(
         "--prefix",
@@ -90,18 +73,20 @@ def add_windows(lf: pl.LazyFrame, window_size: int) -> pl.LazyFrame:
     )
 
 
-def get_allele_count_expr(populations: list[str]):
-    for pop in populations:
-        yield (
-            f"{pop}_F: "
-            + pl.col(f"{pop}_F_RO").cast(pl.String)
-            + " / "
-            + pl.col(f"{pop}_F_AO").cast(pl.String)
-            + f"<br>{pop}_M: "
-            + pl.col(f"{pop}_M_RO").cast(pl.String)
-            + " / "
-            + pl.col(f"{pop}_M_AO").cast(pl.String)
-        ).alias(pop)
+def format_count_per_sample(sample_cols: list[str]):
+    expr = ( pl.lit("Counts (REF / ALT / ...):<br>") )
+    for sample in sample_cols:
+        ad_col = f"{sample}_AD"
+        expr += (
+            pl.lit(f"{sample}: ")
+            + pl.when(
+                pl.col(ad_col).is_not_null()
+            ).then(
+                pl.col(ad_col).list.join(" / ")
+            ).otherwise(pl.lit(""))tuple val("${task.process}"), val('python'),       eval("python3 --version | sed 's/Python //'"),                         topic: versions
+            + pl.lit("<br>")
+        )
+    yield expr.alias("allele_counts")
 
 
 def get_window_allele_count_expr(populations: list[str]):
@@ -158,35 +143,36 @@ def main():
         raise TypeError(f"Could not cast {args.window_size} to integer.")
 
     logger.info("Parsing VCF file")
-    variant_lf = pl.scan_parquet(args.variant_file)
+    vcf_lf = parse_vcf_data(args.vcf_file)
 
     logger.info("Parsing p-values")
     pvalues_lf = parse_pvalues(args.pvalue_file)
 
-    logger.info("Parsing allele counts")
-    RO_lf = pl.scan_parquet(args.RO_file).select(pl.all().name.suffix("_RO"))
-    AO_lf = pl.scan_parquet(args.AO_file).select(pl.all().name.suffix("_AO"))
+    AD_index = get_position_in_format(vcf_lf, "AD")
+    sample_cols = vcf_lf.collect_schema().names()[9:]
 
-    RO_cols = RO_lf.collect_schema().names()
-    AO_cols = AO_lf.collect_schema().names()
-
-    design_df = pl.read_csv(args.design_file)
-    populations = design_df["population"].unique().to_list()
+    logger.info("Extracting allele depths")
+    for sample in sample_cols:
+        vcf_lf = vcf_lf.with_columns(
+            pl.col(sample)
+            .str.split(":").list[AD_index]
+            .replace(".", None)
+            .str.split(",")
+            .alias(f"{sample}_AD")
+        )
 
     logger.info("Associating SNPs to windows")
-    variant_lf = add_windows(variant_lf, window_size)
+    vcf_lf = add_windows(vcf_lf, window_size)
 
-    nb_variants = get_length(variant_lf)
+    nb_variants = get_length(vcf_lf)
     nb_pvalues = get_length(pvalues_lf)
     if nb_variants != nb_pvalues:
         raise ValueError(
             f"Number of variants ({nb_variants}) and number of pvalues ({nb_pvalues}) do not match."
         )
 
-    lf = pl.concat([variant_lf, pvalues_lf, RO_lf, AO_lf], how="horizontal")
+    lf = pl.concat([vcf_lf, pvalues_lf], how="horizontal")
 
-    logger.info("Getting list of contigs")
-    contigs = get_chromosomes(lf)
 
     logger.info("Computing total depth")
     lf = add_total_depth(lf)
@@ -194,83 +180,50 @@ def main():
     logger.info(
         f"Computing quantile {QUANTILE} of pvalue for each pair of contig & window"
     )
-    chrom_window_files = []
-    chrom_variant_files = []
-    for i, contig in tqdm(enumerate(contigs), total=len(contigs)):
-        contig_lf = lf.filter(pl.col("CHROM") == contig)
 
-        variant_lf = (
-            contig_lf.with_columns(get_allele_count_expr(populations))
-            .with_columns(
-                pl.concat_str(populations, separator="<br>").alias("allele_counts")
-            )
-            .rename({"CHROM": "chromosome", "POS": "position", "QUAL": "quality"})
-            .select(
-                [
-                    "chromosome",
-                    "position",
-                    "pvalue",
-                    "quality",
-                    "total_depth",
-                    "allele_counts",
-                ]
-            )
+    logger.info("Saving variants")
+    variant_lf = (
+        lf
+        .with_columns(format_count_per_sample(sample_cols))
+        .rename({"CHROM": "chromosome", "POS": "position", "QUAL": "quality"})
+        .select(
+            [
+                "chromosome",
+                "position",
+                "pvalue",
+                "quality",
+                "total_depth",
+                "allele_counts",
+            ]
         )
-
-        aggregated_lf = (
-            contig_lf.group_by(  # inserting CHROM in the group_by in order to get it back after the agg
-                ["CHROM", "window"]
-            )
-            .agg(
-                pl.col("pvalue").quantile(QUANTILE),
-                pl.col("QUAL").mean().alias("quality"),
-                pl.col("total_depth").mean(),
-                pl.col(RO_cols + AO_cols).mean().name.suffix("_mean"),
-                pl.col(RO_cols + AO_cols).std().name.suffix("_std"),
-            )
-            .with_columns(get_window_allele_count_expr(populations))
-            .with_columns(
-                pl.concat_str(populations, separator="<br>").alias("allele_counts")
-            )
-            .rename({"CHROM": "chromosome", "window": "position"})
-            .select(
-                [
-                    "chromosome",
-                    "position",
-                    "pvalue",
-                    "quality",
-                    "total_depth",
-                    "allele_counts",
-                ]
-            )
-        )
-
-        chrom_variant_file = f"chrom_{i}.variants.parquet"
-        variant_lf.sink_parquet(chrom_variant_file)
-        chrom_variant_files.append(chrom_variant_file)
-
-        chrom_window_file = f"chrom_{i}.grouped_variants.parquet"
-        aggregated_lf.sink_parquet(chrom_window_file)
-        chrom_window_files.append(chrom_window_file)
-
-    logger.info("Concatenating data from all chromosomes")
-    chrom_variant_lfs = [pl.scan_parquet(file) for file in chrom_variant_files]
-    variant_lf = pl.concat(chrom_variant_lfs, how="vertical")
-
-    chrom_window_lfs = [pl.scan_parquet(file) for file in chrom_window_files]
-    window_lf = pl.concat(chrom_window_lfs, how="vertical")
-
-    logger.info("Saving data")
+    )
     variant_lf.sink_parquet(f"{args.prefix}.{VARIANTS_OUTFILE_SUFFIX}")
+
+    logger.info("Saving windows")
+    window_lf = (
+        lf.group_by(["CHROM", "window"])
+        .agg(
+            pl.col("pvalue").quantile(QUANTILE),
+            pl.col("QUAL").mean().alias("quality"),
+            pl.col("total_depth").mean(),
+        )
+        #.with_columns(get_window_allele_count_expr(populations))
+        #.with_columns(pl.concat_str(populations, separator="<br>").alias("allele_counts"))
+        .rename({"CHROM": "chromosome", "window": "position"})
+        .select(
+            [
+                "chromosome",
+                "position",
+                "pvalue",
+                "quality",
+                "total_depth",
+                #"allele_counts",
+            ]
+        )
+    )
     window_lf.sink_parquet(f"{args.prefix}.{GROUPED_VARIANTS_OUTFILE_SUFFIX}")
 
-    logger.info("Removing temporary files")
-    for file in chrom_variant_files:
-        Path(file).unlink()
-    for file in chrom_window_files:
-        Path(file).unlink()
 
-    # quantile_lf.show_graph(engine="streaming", plan_stage="physical")
 
 
 if __name__ == "__main__":
--- bin/common.py
+++ bin/common.py
@@ -20,3 +20,19 @@ def parse_vcf_data(vcf_file: Path) -> pl.LazyFrame:
     return pl.scan_csv(
         vcf_file, separator="\t", has_header=True, comment_prefix="##", low_memory=True
     ).rename({"#CHROM": "CHROM"})
+
+
+def get_position_in_format(vcf_lf: pl.LazyFrame, info: str) -> int:
+    fmt_series = vcf_lf.select("FORMAT").collect().to_series()
+    if fmt_series.unique().len() > 1:
+        raise ValueError(f"More than one format found: {fmt_series.unique()}")
+    fmt = fmt_series.unique().item()
+    return fmt.split(":").index(info)
+
+
+def extract_counts(
+    vcf_lf: pl.LazyFrame, sample_cols: list[str], idx: int
+) -> pl.LazyFrame:
+    return vcf_lf.select(
+        pl.col(sample_cols).str.split(":").list[idx].replace(".", None).cast(pl.Int64)
+    )
